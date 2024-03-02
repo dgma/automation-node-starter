caa987b1192bcd7ce71fd1304234836374e5680d
Dzmitry Lahunouski <killroy192@gmail.com>
2024-03-02 22:33:33 +0000

    migrate to viem

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import * as dotenv from "dotenv";
-import { ethers } from "ethers";
+import { createClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { hardhat } from "viem/chains";
 import { example } from "./example";
 
 const config = dotenv.config({ path: "./.env.local" });
 
-const wallet_pk = config?.parsed?.NODE_PK || process.env.NODE_PK;
+const node_pk = config?.parsed?.NODE_PK || process.env.NODE_PK;
 const rpc = config?.parsed?.RPC || process.env.RPC;
 
-if (!wallet_pk || !rpc) {
+if (!node_pk || !rpc) {
   throw new Error("Configuration mistake. Check your env file");
 }
 
-console.log("wallet_pk", wallet_pk);
-console.log("rpc", rpc);
-
-const provider = new ethers.providers.JsonRpcProvider(rpc);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const wallet = new ethers.Wallet(wallet_pk, provider);
+const nodeClient = createClient({
+  account: privateKeyToAccount(node_pk as `0x${string}`),
+  chain: hardhat,
+  transport: http(rpc),
+});
 
 console.log(example());
