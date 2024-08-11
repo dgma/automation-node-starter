5f0cabed5f11dfbaca62d5d0005899eb0ef01028
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2024-08-11 23:49:20 +0100

    update secrets

--- src/clients.ts
+++ src/clients.ts
@@ -1,9 +1,9 @@
-import { createClient, createPublicClient, webSocket, http } from "viem";
+import { createClient, createPublicClient, webSocket, http, Hex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import type { SupportedChains } from "./chains";
 import chains from "./chains";
 
-export const node_pk = process.env.NODE_PK;
+export const node_pk = process.env.PRIVATE_KEY as Hex;
 export const rpc = process.env.RPC;
 export const ws_rpc = process.env.WS_RPC;
 export const network = (process.env.NETWORK || "localhost") as SupportedChains;
@@ -19,7 +19,7 @@ export const httpPublicClient = createPublicClient({
 });
 
 export const httpClient = createClient({
-  account: privateKeyToAccount(node_pk as `0x${string}`),
+  account: privateKeyToAccount(node_pk),
   chain: chains[network],
   transport: http(rpc),
 });
